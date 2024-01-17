d1e0fc7577d4b5d6aad841d8054dea9f97e7bac7
Darwinpz <dpilaloazea@gmail.com>
2024-01-17 17:23:25 -0500

    Biometrico revisar

--- app/src/main/java/com/jonathanaguilar/datareflix/MainActivity.java
+++ app/src/main/java/com/jonathanaguilar/datareflix/MainActivity.java
@@ -11,6 +11,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -103,10 +104,9 @@ public class MainActivity extends AppCompatActivity {
                 public void onAuthenticationSucceeded(@NonNull BiometricPrompt.AuthenticationResult result) {
                     super.onAuthenticationSucceeded(result);
 
-                    String id = preferences.getString("uid_biometric","").toString();
+                    String id = preferences.getString("uid_biometric","");
 
                     if(!id.isEmpty()) {
-
                         databaseReference.child("usuarios").child(id).addValueEventListener(new ValueEventListener() {
                             @Override
                             public void onDataChange(@NonNull DataSnapshot snapshot) {
@@ -194,6 +194,7 @@ public class MainActivity extends AppCompatActivity {
                                         SharedPreferences.Editor editor = preferences.edit();
 
                                         if(preferences.getString("uid_biometric","").isEmpty()){
+                                            Log.e("PRUEBA","ENTRAAA");
                                             dialog.ocultar_mensaje();
                                             alertDialog.crear_mensaje("¿Desea Agregar este usuario al Biométrico?", "Accede con un solo usuario, directamente con Biometría", builder -> {
                                                 builder.setPositiveButton("Aceptar", (dialogInterface, i) -> {
